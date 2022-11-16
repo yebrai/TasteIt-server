94ef4ba3abed18ec6acda6a2006f946a66d4ee2d
yebrai <ivansevv@gmail.com>
2022-11-16 11:42:26 +0100

    stripe done

--- routes/stripe.routes.js
+++ routes/stripe.routes.js
@@ -4,7 +4,26 @@ const Stripe = require("stripe")
 const stripe = new Stripe(process.env.STRIPE)
 
 // POST /api/stripe 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
+    const {id, amount} = req.body
+
+    const newPayment = {
+        amount,
+        currency: "EU",
+        description: "Pedido",
+        payment_method: id,
+        confim: true
+    }
+
+    try {
+        const payment = await stripe.paymentIntents.create(newPayment)
+        console.log(payment);
+
+        res.status(200).json("Compra registrada")
+    } catch (error) {
+        console.log(error)
+        res.json({message: error})
+    }
 
 })
 
